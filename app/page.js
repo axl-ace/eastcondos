ba6471e1b8918ed64872f24c7bb61a0a83ee9627
froster-ace <terzero78@gmail.com>
2024-04-05 04:31:17 +0300

    Deploy to vercel

--- app/page.js
+++ app/page.js
@@ -1,24 +1,34 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Achievements from './components/Achievements'
-import Discover from './components/Discover'
-import International from './components/International'
-import Latest from './components/Latest'
-import Testimonials from './components/Testimonials'
+"use client";
+
+import { useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Achievements from "./components/Achievements";
+import Discover from "./components/Discover";
+import International from "./components/International";
+import Latest from "./components/Latest";
+import Testimonials from "./components/Testimonials";
 
 const Page = () => {
-  return (
-    <div className='w-full h-auto px-6'>
-      <Navbar />
-      <Hero />
-      <Achievements />
-      <Discover />
-      <International />
-      <Latest />
-      <Testimonials />
-    </div>
-  )
-}
+	useEffect(() => {
+		(async () => {
+			const LocomotiveScroll = (await import("locomotive-scroll")).default;
 
-export default Page
+			const locomotiveScroll = new LocomotiveScroll();
+		})();
+	}, []);
+
+	return (
+		<div className="w-full h-auto px-6">
+			<Navbar />
+			<Hero />
+			<Achievements />
+			<Discover />
+			<International />
+			<Latest />
+			<Testimonials />
+		</div>
+	);
+};
+
+export default Page;
